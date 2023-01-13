f8cf5a3f5a9500023ba04a17ef7fc72b6ec06244
preritagrawal06 <agrawalprerit780@gmail.com>
2023-01-13 14:25:45 +0530

    navbar and components added

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -4,15 +4,21 @@ import styles from "../styles/contactus.module.css";
 const ContactUs = () => {
     return ( 
         <div className={styles.contactUs} >
-            <h1 className={styles.title} >Contact Us</h1>
+            <h1 className={styles.title} >Administration</h1>
             <div className={styles.container} >
                 <div className={styles.details} >
+                    <p>Vice-Chancellor</p>
+                    <p>Dean of Student Welfare</p>
+                    <p>Vice President Sports Society</p>
                     <p>Coordinator Illumina</p>
                     <p>Coordinator Athletic Club</p>
                     <p>Asst-Coordinator Illumina</p>
                     <p>Asst-Corordinator Athletic Club</p>
                 </div>
                 <div className={styles.details} >
+                    <p>Prof. Bansidhar Majhi</p>
+                    <p>Prof. Sanjaya Kumar Patro</p>
+                    <p>Mr. Anand Kumar Behera</p>
                     <p>Sambhunath Biswal  (1234567890)</p>
                     <p>Tanmaya Naik  (1234567890)</p>
                     <p>Sourav Panda  (1234567890)</p>
