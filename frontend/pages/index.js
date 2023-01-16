d765b14ebaecb46977e31a473a920331e1152fba
preritagrawal06 <agrawalprerit780@gmail.com>
2023-01-17 02:35:23 +0530

    minor changes

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import AllEvents from '../components/AllEvents'
-import ContactUs from '../components/COntactUs'
+import ContactUs from '../components/ContactUs'
 import Hero from '../components/hero'
 import Gallery from '../components/Gallery'
 const inter = Inter({ subsets: ['latin'] })
