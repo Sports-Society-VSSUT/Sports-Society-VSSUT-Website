73859d11baccb6ae763408ccea40ec2c3a1e773a
Prerit Agrawal <98153850+preritagrawal06@users.noreply.github.com>
2023-01-17 03:39:12 +0530

    Update Footer.js

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return ( 
           <div className="footer">
             <div className="insta">
-                <a href="#"><img src="insta.png" alt="insta logo" />@illumina_vssut</a>
+                <a href="https://instagram.com/illumina.vssut?igshid=YmMyMTA2M2Y="><img src="insta.png" alt="insta logo" />@illumina_vssut</a>
             </div>  
             <div className="enigma">
                 <p>Developed By: <a href="https://enigmavssut.com/" target="_blank" >Enigma </a> </p>
