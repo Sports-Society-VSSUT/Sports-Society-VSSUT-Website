e2a10625bb3b5b12ed7c87f5ec83811d7bb27e18
Soham Samantaray <ssoham414@gmail.com>
2023-01-21 00:30:42 +0530

    Autoplay feature added

--- frontend/components/hero.js
+++ frontend/components/hero.js
@@ -16,7 +16,8 @@ const Hero = () => {
     <h2>
         About Us
     </h2>
-    <p>Veer Surendra Sai University of Technology (VSSUT) organizes its yearly sports fest named as ILLUMINA with great elation and valor.The two days sports extravaganza of our University kicks-off with a gallant show. The inaugural day starts off with the lighting of the torch at Burla Temple which kindles the spirit of sportsmanship and togetherness among all the VSSUTians. A number of sports enthusiasts turns up to become a part of the torch march from the temple to the University’s ground lead by Sports Secretary,VSSUT,Burla. They are greeted by a large crowd (students, staff and local people) encouraging and applauding them</p>
+    <p>Sports Society focuses on promoting and fostering sports among the students, faculty and staff of the university. The society aims to provide a platform for students to participate in a wide range of sports activities including both indoor and outdoor sports, and to develop and improve their physical and mental well-being.
+    The Sports Society of VSSUT organizes various sports events, tournaments, and competitions throughout the year such as football, cricket, volleyball, badminton, and many more. Vssut also has sports clubs like VSSUT Cricket Club- VCC, VSSUT Shuttlers-VSS, VSSUT Basketball Club- VBC, VSSUT football club vfc, VSSUT Volleyball Club which provide a highly competitive environment where the talent of students is furnished and sharpened.</p>
     </div>
     <div className={styles.illumina}>
       <h3>Illumina</h3>
