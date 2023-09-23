c28b9a230a5b226d3bf308974f78633c0bdef383
preritagrawal06 <agrawalprerit780@gmail.com>
2023-09-24 00:52:20 +0530

    websocket implemented

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,31 @@ const mongoose = require('mongoose')
 const eventRoute = require('./Routes/eventRoute')
 const cors = require('cors')
 const dotenv = require('dotenv').config()
+const http = require('http')
+const {Server} = require('socket.io')
+
+
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+})
 
 app.use(cors())
-
 app.use(express.json())
-
 app.use(eventRoute)
+
+io.on('connection', (socket)=>{
+    socket.emit('connection-done')
+    socket.on('update score', (data)=>{
+        //console.log(data)
+        io.emit("live-score", data)
+    })
+    socket.on("end game", ()=>{
+        io.emit("close score")
+    })
+})
 
 mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGODB)
@@ -19,3 +38,5 @@ mongoose.connect(process.env.MONGODB)
         })
     })
     .catch((err)=>{console.log(err)})
+
+server.listen(5000, ()=>{console.log("connected to http server")})
