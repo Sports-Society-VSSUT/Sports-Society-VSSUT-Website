34ef72afe372f120e95f19b36cba4102fd673060
preritagrawal06 <agrawalprerit780@gmail.com>
2023-09-24 01:04:59 +0530

    websocket added

--- score/src/home.js
+++ score/src/home.js
@@ -4,33 +4,26 @@ import { useNavigate } from 'react-router-dom';
 const Homepage = () => {
 
     const [event, setEvent] = useState("");
+
     
     const navigate = useNavigate()
 
-    const handleSubmit = async(e)=>{
+    const handleSubmit = (e)=>{
         e.preventDefault()
-        const test = {event}
-        
-        const response = await fetch('https://illumina-live-score-backend.onrender.com/api', {
-            method: "POST",
-            body: JSON.stringify(test),
-            headers:{
-                "Content-Type":"application/json",
-            },
-        })
-        const json = await response.json();
-        if(response.ok){
-            console.log("new event added", json)
-            navigate('/score', {state:{event: event}})
-        }
-        
+        navigate('/score', {state:{event: event}})
+    }
+
+    const handleChange =  (e)=>{
+        const value = e.target.value
+        console.log(value)
+        setEvent(value)
     }
 
     return ( 
         <div>
             <form className="form" onSubmit={handleSubmit} action="">
                 <label htmlFor="">Event</label>
-                <select name="event" id="event" onChange={(e)=>(setEvent(e.target.value))} >
+                <select name="event" id="event" onChange={handleChange}>
                     <option value="none">Select Event</option>
                     <option value="kabaddi">Kabaddi</option>
                     <option value="khokho">Kho-Kho</option>
